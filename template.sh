7d8227db88228e699483205699c124f62ba94e41
Arindam Chandra <prayasarindam@gmail.com>
2026-03-06 22:04:02 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+# cretae dir
+mkdir -p src
+mkdir -p reseach
+# file
+touch src/_init_.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.py
+
+echo "Directory and files created successfully"
+
