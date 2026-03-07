689d5ce84b78059b02cb396c1b9b05dd95637927
Arindam Chandra <prayasarindam@gmail.com>
2026-03-07 09:23:01 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # my_project
+clone the repository
+```bash
+git clone https://github.com/Arindam321all/my_project.git
+env
+
+
+
+pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages, setup
+
+setup(
+    name='my_project',
+    version='0.1.0',
+    author="Boktiar Ahmed Bappy",
+    author_email="entbappy73@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    )
